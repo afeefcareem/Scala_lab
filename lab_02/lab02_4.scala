711ccf1b012fc459f2f3ffc9282a65f113d02c7a
afeefcareem <78678227+afeefcareem@users.noreply.github.com>
2023-06-19 22:10:21 +0530

    Update lab02_4.scala

--- lab_02/lab02_4.scala
+++ lab_02/lab02_4.scala
@@ -2,5 +2,16 @@ package lab_02
 
 object lab02_4 extends App {
 
+  def attendees(price:Int):Int=120+(15-price)/5*20
+  def revenue(price: Int): Int = attendees(price)*price
+  def cost(price:Int):Int=500+attendees(price)
+
+  def profit(price: Int): Int = revenue(price) - cost(price)
+
+  println(profit(25), profit(30), profit(35), profit(40))
+
+  val maxProfit = List(profit(25), profit(30), profit(35), profit(40)).max
+  println(s"The maximum profit is: $maxProfit")
+
 
 }
